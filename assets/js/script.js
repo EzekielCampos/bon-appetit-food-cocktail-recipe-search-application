3ae1fc5fba3f48739847cb795df15e73b236b99f
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-06-12 13:00:28 -0700

    created a function to display api info into a card to be appended to the main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,42 @@ const cocktailsResultsBox = $("#cocktails-content");
 const cocktailInput = $("#drink-level");
 // This array will hold the favorite drinks names that the user have saved
 const favoriteCocktails = JSON.parse(localStorage.getItem("drinks")) || [];
+
+
+function createCocktailCard(data){
+
+  console.log(`Hello ${data.drinks[0].strIngredient1}`);
+  let cocktailsObject = data.drinks[0];
+  
+  const cocktailHtml = `
+      <div class="card mt-3">
+  <div class="card-content">
+    <div class="columns">
+      <div class="column is-one-third">
+        <img src="${cocktailsObject.strDrinkThumb}" style="width:150%">
+      </div>
+      <div class="column">
+        <p class="title">${cocktailsObject.strDrink}</p>
+        <p class="subtitle">${cocktailsObject.strAlcoholic}</p>
+        <ul class="ingredients-list">
+          <li>${cocktailsObject.strIngredient1}</li>
+        </ul>
+      </div>
+    </div>
+          <p class="instructions">
+        ${cocktailsObject.strInstructions}
+          </p>
+        </div>
+        <footer class="card-footer">
+          <button class="card-footer-item">Add to Favorites</button> 
+        </footer>
+      </div>
+    `;
+
+cocktailsResultsBox.append(cocktailHtml);
+
+}
+
 function retrieveCocktailsInfo(event) {
   // prevent page from refreshing
   event.preventDefault();
@@ -12,26 +48,25 @@ function retrieveCocktailsInfo(event) {
   const urlAlcoholFilter = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${cocktailInput.val()}`;
   fetch(urlAlcoholFilter).then(function (response) {
     console.log(response.status);
-    response
-      .json()
+    return response.json()
       .then(function (data) {
         // Object returned an array of the drinks from the category selected
         console.log(data.drinks.length);
         // This variable gives access to the drinks array that is inside the object that was returned
         let drinksObjectArray = data.drinks;
         // This loop will get the id value of the drink and call another api to get information that will be used to on the page
-        for (index = 0; index < drinksObjectArray.length; index++) {
+        for (let index = 0; index < drinksObjectArray.length; index++) {
           // This will url will be used to fetch the information with it's id number
           let searchCocktailById = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinksObjectArray[index].idDrink}`;
           console.log(searchCocktailById);
           fetch(searchCocktailById).then(function (response) {
             console.log(response.status);
-            response
-              .json()
+            return response.json()
               .then(function (data) {
                 console.log(data);
                 console.log(data.drinks[0].strIngredient1);
-                IfyFunction(data);
+                let test =1;
+                createCocktailCard(data);
               })
               .catch(function (error) {
                 console.log(error);
@@ -43,7 +78,7 @@ function retrieveCocktailsInfo(event) {
         console.log(error);
       });
   });
-  modal.dialog("close");
+  modal2.dialog("close");
 }
 // Creates a modal to find a cocktail button is clicked
 let modal2 = $("#cocktail-form").dialog({
@@ -193,9 +228,11 @@ $(document).ready(function () {
         // close the modal dialogue
         modal.dialog("close");
       },
-
-      // Button to cancel and close the dialogue
-      Close: getRandomFood,
+"Surprise me": function () {
+        getRandomFood();
+        modal.dialog("close");
+      },
+    
     },
     // Function to handle the dialogue close event
     close: function () {
